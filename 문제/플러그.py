53a81d96e9e90456d1c07e234b95f7ab876522aa
casylm <casylm0619@gmail.com>
2024-05-05 15:44:35 +0900

    20240505_두번째

---
+++ 문제/플러그.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+cnt = 0
+for _ in range(n):
+    m = int(input())
+    cnt += m-1
+
+cnt +=1
+
+print(cnt)
+
+#제출 번호	아이디	문제	결과	메모리	시간	언어	코드 길이	제출한 시간
+#77891069	mlac	 2010	맞았습니다!!	110832	268	PyPy3 / 수정	101	18초 전
